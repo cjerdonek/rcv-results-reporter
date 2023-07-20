072bfbf6bac5596674ab2e5288b8d62c669dc40f
Chris Jerdonek <chris.jerdonek@gmail.com>
2023-07-20 05:06:05 -0700

    Start parsing multiple files.

--- src/rcvresults/main.py
+++ src/rcvresults/main.py
@@ -5,7 +5,9 @@ $ python src/rcvresults/main.py
 
 """
 
+import glob
 import logging
+from pathlib import Path
 
 import rcvresults.parsing as parsing
 
@@ -14,9 +16,20 @@ _log = logging.getLogger(__name__)
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-    results = parsing.parse_workbook('data/2022-11-08/da_short.xlsx')
-    raise RuntimeError(results)
+    log_format = '[{levelname}] {name}: {message}'
+    logging.basicConfig(format=log_format, style='{', level=logging.INFO)
+
+    paths = sorted(glob.glob('data/2022-11-08/*.xlsx'))
+    for path in paths:
+        path = Path(path)
+        if path.name.startswith('~'):
+            _log.warning(f'skipping temp file: {path}')
+            continue
+
+        _log.info(f'parsing: {path}')
+        results = parsing.parse_workbook(path)
+        candidates = results['candidates']
+        _log.info(f'found: {len(candidates)} candidates')
 
 
 if __name__ == '__main__':
