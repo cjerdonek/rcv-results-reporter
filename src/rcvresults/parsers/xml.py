6d6e8c345de2645fc838ce4faab857b20e8d63c6
Chris Jerdonek <chris.jerdonek@gmail.com>
2023-07-22 16:17:46 -0700

    Start parsing the contest_name from the xml.

--- src/rcvresults/parsers/xml.py
+++ src/rcvresults/parsers/xml.py
@@ -3,6 +3,8 @@ Supports parsing Dominion XML (.xml) RCV result reports.
 """
 
 import logging
+import xml.etree.ElementTree as ET
+
 
 _log = logging.getLogger(__name__)
 
@@ -12,4 +14,19 @@ def parse_xml_file(path):
     """
     Parse an XML file, and return a dict of results.
     """
-    raise NotImplementedError('xml not implemented yet')
+    tree = ET.parse(path)
+    root = tree.getroot()
+
+    # Parse out the contest_name.
+    rcv_static_data = root.find('{RcvShortReport}RcvStaticData')
+    report = rcv_static_data.find('{RcvShortReport}Report')
+    tablix2 = report.find('{RcvShortReport}Tablix2')
+    # TODO: will this always be "Textbox24"?
+    contest_name = tablix2.attrib['Textbox24']
+    metadata = {
+        'contest_name': contest_name,
+    }
+    results = {
+        '_metadata': metadata,
+    }
+    return results
