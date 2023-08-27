2b0f64778878977c43883523ab501a316fafc187
Chris Jerdonek <chris.jerdonek@gmail.com>
2023-08-27 11:17:44 -0400

    Add link to new round-by-round results pages.

--- src/rcvresults/demo.py
+++ src/rcvresults/demo.py
@@ -24,6 +24,7 @@ from rcvresults.utils import CURRENT_LANG_KEY, LANGUAGES
 _log = logging.getLogger(__name__)
 
 TEMPLATE_NAME_RCV_DEMO = 'index-all-rcv.html'
+RCV_SNIPPETS_DIR_NAME = 'rcv-snippets'
 
 CONFIG_DIR = Path('config')
 
@@ -155,6 +156,26 @@ def make_test_index_html(output_dir, snippets_dir, js_dir):
     make_index_html(output_dir, template=template, js_dir=js_dir, env=env)
 
 
+def _get_rounds_report_url(context, election, base_name):
+    """
+    Return the URL to an html round-by-round report for a contest, as a
+    relative URL. For example, "rcv-snippets/2022-11-08/da_short-rounds-en.html".
+
+    Args:
+      election_dir_name: for example, "2022-11-08".
+      base_name: for example, "da_short".
+    """
+    lang_code = context[CURRENT_LANG_KEY]
+    dir_name = election['dir_name']
+    # TODO: eliminate the need to repeat the string "rounds"?
+    #  See also: HTML_SUFFIXES.
+    base_name = f'{base_name}-rounds'
+    file_name = utils.make_rcv_snippet_name(base_name, lang_code=lang_code)
+    rel_path = Path(RCV_SNIPPETS_DIR_NAME) / dir_name / file_name
+
+    return str(rel_path)
+
+
 def _get_contest_summary_path(context, election, base_name):
     """
     Return the path to an html summary file for a contest, as a relative
@@ -230,6 +251,7 @@ def make_rcv_demo(
 
     global_vars = {
         'elections': elections,
+        'get_rounds_url': jinja2.pass_context(_get_rounds_report_url),
         'get_summary_path': jinja2.pass_context(_get_contest_summary_path),
         'iter_contests': iter_contests,
         'iter_languages': jinja2.pass_context(rendering.iter_languages),
@@ -254,7 +276,7 @@ def main():
     output_dir = DEFAULT_HTML_OUTPUT_DIR
     # This is the parent directory to which to write the intermediate
     # RCV HTML snippets.
-    snippets_dir = output_dir / 'rcv-snippets'
+    snippets_dir = output_dir / RCV_SNIPPETS_DIR_NAME
 
     # We have a symlink at "data/output-html/js" that points to
     # "sample-html/2022-11-08/js" (as a relative path).
